853cf4232b3e827e45402b90e280743509d3282f
Rishabh Sinha <sinharishabh2002@gmail.com>
2024-12-18 23:15:04 +0530

    Add user modal

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { UserDataService } from '../../services/user-data.service';
 import { FormsModule } from '@angular/forms';
+import { Dialog } from 'primeng/dialog';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss'],
-  imports: [FormsModule],
+  imports: [FormsModule, Dialog, CommonModule, ButtonModule],
 })
 export class FiltersComponent {
   name = '';
   workoutType = '';
   minutes = 0;
+  visible: boolean = false; // Controls visibility of the dialog
+
+  
 
   constructor(private userDataService: UserDataService) {}
+
+  showDialog(): void {
+    this.visible = true; // This will open the dialog
+  }
 
   addUser(): void {
     if (this.name && this.workoutType && this.minutes > 0) {
@@ -24,6 +34,7 @@ export class FiltersComponent {
         this.minutes
       );
       this.clearForm();
+      this.visible = false;
     }
   }
 
