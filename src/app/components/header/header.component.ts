e5184077d554aef0ca9efebf3e22576f8537baac
Rishabh Sinha <sinharishabh2002@gmail.com>
2024-12-18 22:40:14 +0530

    primeNg Setup

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
