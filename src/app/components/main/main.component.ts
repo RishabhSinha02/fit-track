aa41489c915e1d2be87e3957315aeb7801d057f2
Rishabh Sinha <sinharishabh2002@gmail.com>
2024-12-19 07:14:59 +0530

    Individual User Data

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -4,6 +4,7 @@ import { TableComponent } from "../table/table.component";
 import { FiltersComponent } from "../filters/filters.component";
 import { Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserData, UserDataService } from '../../services/user-data.service';
 
 @Component({
   selector: 'app-main',
@@ -14,4 +15,15 @@ import { CommonModule } from '@angular/common';
 })
 export class MainComponent {
   @Input() sidebarSelected: string = 'All';
+  
+  users: UserData[] = [];
+
+  constructor(private userDataService: UserDataService) {}
+
+  ngOnInit(): void {
+    // Subscribe to the users$ observable to get the user list
+    this.userDataService.users$.subscribe((users) => {
+      this.users = users; // Assign the users list to the component property
+    });
+}
 }
